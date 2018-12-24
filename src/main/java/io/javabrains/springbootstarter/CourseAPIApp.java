f85907bede1d2f749e5e4934499a36777335cdf7
Drew Coleman <dcolem12@my.westga.edu>
2018-12-23 21:36:20 -0500

    The main now successfully sets up a server - it less than a blank web page but it does boot up

---
+++ src/main/java/io/javabrains/springbootstarter/CourseAPIApp.java
@@ -0,0 +1,15 @@
+package io.javabrains.springbootstarter;
+
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
+public class CourseAPIApp {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(CourseAPIApp.class, args);
+		
+	}
+}
